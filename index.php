7ef7fe43d43db7d50847c100ff277b9af14719f1
LeoX DEV <leoqonline@gmail.com>
2024-01-01 18:46:41 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>LeoX DEV</title>
+  <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+  <img src="https://avatars.githubusercontent.com/u/84378319" alt="LeoX DEV" id="profile-pic">
+  <div id="title">
+    <h1>LeoX DEV</h1>
+    <h2>Full Stack Developer</h2>
+    <div id="skills">
+      <div>
+        <h4>Programming languages</h4>
+        <a href="#html"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/html5.svg" class="html"></a>
+        <a href="#css"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/css3.svg" class="css"></a>
+        <a href="#js"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/javascript.svg" class="js"></a>
+        <a href="#v"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/v.svg" class="v"></a>
+        <a href="#php"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/php.svg" class="php"></a>
+        <a href="#scss"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/sass.svg" class="scss"></a>
+        <a href="#java"><img src="https://cdn.jsdelivr.net/npm/simple-icons@6/icons/java.svg" class="java"></a>
+        <a href="#py"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/python.svg" class="py"></a>
+      </div>
+      <div>
+        <h4>Tools/Frameworks</h4>
+        <a href="#node"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/nodedotjs.svg" class="node"></a>
+        <a href="#npm"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/npm.svg" class="npm"></a>
+        <a href="#vscode"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/visualstudiocode.svg" class="vscode"></a>
+      </div>
+      <div>
+        <h4>Other</h4>
+        <a href="#deb"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/debian.svg" class="deb"></a>
+        <a href="#apache"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/apache.svg" class="apache"></a>
+        <a href="#nginx"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/nginx.svg" class="nginx"></a>
+        <a href="#mariadb"><img src="https://cdn.jsdelivr.net/npm/simple-icons@10/icons/mariadb.svg" class="mariadb"></a>
+      </div>
+    </div>
+  </div>
+
+  <div id="projects"></div>
+
+  <div id="skill-exp">
+    <div id="langs">
+      <h2>Programming languages</h2>
+      <div id="html">
+        <h3>HTML</h3>
+        <h4><?php echo (ceil((time() - 1604854800) / 31557600)); ?> years</h4>
+        <p>I learned HTML to create simple websites from <a href="https://www.w3schools.com/html/html_basic.asp">W3Schools</a>.</p>
+      </div>
+      <div id="css">
+        <h3>CSS</h3>
+        <h4><?php echo (ceil((time() - 1630360800) / 31557600)); ?> years</h4>
+        <p>I learned CSS with <a href="https://www.rheinwerk-verlag.de/schroedinger-lernt-html5-css-und-javascript/">a book</a>.</p>
+      </div>
+      <div id="js">
+        <h3>JavaScript</h3>
+        <h4><?php echo (ceil((time() - 1630360800) / 31557600)); ?> years</h4>
+        <p>
+          I also learned JavaScript from <a href="https://www.rheinwerk-verlag.de/schroedinger-lernt-html5-css-und-javascript/">a book</a>. Now I use it for almost everything and it's the language which I like the most for its syntax.
+        </p>
+      </div>
+      <div id="v">
+        <h3>V</h3>
+        <h4><?php echo (ceil((time() - 1702854000) / 2419200)); ?> month</h4>
+        <p>I learned V after seeing <a href="https://www.youtube.com/watch?v=GDOWfzIPgbc">this video</a>.</p>
+      </div>
+      <div id="php">
+        <h3>PHP</h3>
+        <h4><?php echo (ceil((time() - 1624226400) / 31557600)); ?> years</h4>
+        <p>I learned PHP only because it runs on almost every web server.</p>
+      </div>
+      <div id="scss">
+        <h3>SCSS</h3>
+        <h4><?php echo (ceil((time() - 1693432800) / 2419200)); ?> months</h4>
+        <p>I learned SCSS when I created <a href="https://github.com/donald-org/donald24">a new design for donald.org</a>.</p>
+      </div>
+      <div id="java">
+        <h3>Java</h3>
+        <h4><?php echo (ceil((time() - 1660514400) / 31557600)); ?> year</h4>
+        <p>I learned Java in school and I totally hate it.</p>
+      </div>
+      <div id="py">
+        <h3>Python</h3>
+        <h4><?php echo (ceil((time() - 1614553200) / 31557600)); ?> years</h4>
+        <p>I also learned Python in school, but I haven't wrote much code in it after my course ended.</p>
+      </div>
+    </div>
+    <div id="tools">
+      <h2>Tools/Frameworks</h2>
+      <div id="node">
+        <h3>Node.js</h3>
+        <h4><?php echo (ceil((time() - 1638658800) / 31557600)); ?> years</h4>
+        <p>I learned writing code for Node.js from <a href="https://www.rheinwerk-verlag.de/nodejs-das-umfassende-handbuch/">this book</a> and I use it every day.</p>
+      </div>
+      <div id="npm">
+        <h3>NPM</h3>
+        <h4><?php echo (ceil((time() - 1665957600) / 31557600)); ?> year</h4>
+        <p>I also learned using NPM from <a href="https://www.rheinwerk-verlag.de/nodejs-das-umfassende-handbuch/">this book</a> and I use it almost every day.</p>
+      </div>
+      <div id="vscode">
+        <h3>Visual Studio Code</h3>
+        <h4><?php echo (ceil((time() - 1630360800) / 31557600)); ?> years</h4>
+        <p>I heard much about it on the internet back when I created simple HTML pages, and I absolutely love it now!</p>
+      </div>
+    </div>
+    <div id="other-skills">
+      <h2>Other</h2>
+      <div id="deb">
+        <h3>Debian</h3>
+        <h4><?php echo (ceil((time() - 1614553200) / 31557600)); ?> years</h4>
+        <p>My first device running Debian was a Raspberry Pi. In March 2023, I switched from Windows 11 to Ubuntu and now I use plain Debian with KDE Plasma every day.</p>
+      </div>
+      <div id="apache">
+        <h3>Apache</h3>
+        <h4><?php echo (ceil((time() - 1624226400) / 31557600)); ?> years</h4>
+        <p>I firstly used Apache when I created simple HTML pages, now I have a paid Apache web server.</p>
+      </div>
+      <div id="nginx">
+        <h3>Nginx</h3>
+        <h4><?php echo (ceil((time() - 1677625200) / 2419200)); ?> months</h4>
+        <p>I also use Nginx very often because it's faster than Apache, but not so easy to configure (but I like the config syntax more tbh).</p>
+      </div>
+      <div id="mariadb">
+        <h3>MariaDB</h3>
+        <h4><?php echo (ceil((time() - 1656626400) / 31557600)); ?> years</h4>
+        <p>I use MariaDB often together with PHP and it also runs on my paid web server.</p>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
