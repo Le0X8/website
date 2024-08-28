5ac7b4a3d83b5a72f9b1edd7d313d681c7ebbee1
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-08-28 22:45:01 +0200

    added email

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import he from 'he';
+
+  const mail = he.encode('q3tt3d7mj@mozmail.com', {
+    useNamedReferences: false,
+    allowUnsafeSymbols: true,
+    encodeEverything: true,
+  });
+</script>
+
+{@html `<a href="mailto:${mail}">Email me!</a>`}
