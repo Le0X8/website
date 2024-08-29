c65d52167a19f318f10c9d6f3063b4869759022a
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-08-29 20:28:44 +0200

    Added some content to the about page

--- src/components/Typewriter.svelte
+++ src/components/Typewriter.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	export let text: string;
   export let delay: number = 0;
+	export let speed: number = 0.1;
 
 	const chars = text.split('');
 </script>
 
 <span class="typewriter">
 	{#each chars as char, i (char)}
-		<span style="animation-delay: {delay + i * 0.1}s">{char}</span>
+		<span style="animation-delay: {delay + i * speed}s">{char}</span>
 	{/each}
 </span>
